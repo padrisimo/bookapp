5f79853a2f25f0896a36f6c037ba8a9c3526a6df
padrisimo <nopuedoparardemolar@outlook.com>
2017-08-20 01:46:28 +0200

    conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Clean Code'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquent Ruby'}
+        { title: 'Javascript: The Good Parts', pages: 300 },
+        { title: 'Clean Code', pages: 220 },
+        { title: 'The Dark Tower', pages: 90 },
+        { title: 'Eloquent Ruby', pages: 1 }
     ];
 }
